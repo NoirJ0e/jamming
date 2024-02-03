c348a95b7b5a285ce0f21f0aab52efac1879f3df
Noir Joe <noirjoe@outlook.com>
2024-02-03 19:59:37 +0800

    finish title and search bar style

--- src/components/BeforeLogin/BeforeLogin.jsx
+++ src/components/BeforeLogin/BeforeLogin.jsx
@@ -4,9 +4,17 @@ import NavBar from "../NavBar/NavBar";
 
 function BeforeLogin() {
   return (
-    <div>
-      <h2>Please login to use</h2>
-      <button onClick={Spotify.redirectToSpotifyAuthorization}>Login</button>
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="justify-center text-5xl text-center text-slate-50 m-14">
+        Please login to use
+      </h2>
+      <button
+        onClick={Spotify.redirectToSpotifyAuthorization}
+        type="button"
+        className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Login
+      </button>
     </div>
   );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -12,15 +12,21 @@ function SearchBar({ onSearch }) {
   };
 
   return (
-    <>
+    <div className="flex flex-col items-center justify-center mt-10">
       <input
         placeholder="Enter A Song, Album, or Artist"
         value={keyword}
         onChange={handleKeywordChange}
         type="text"
+        className="text-center text-white bg-transparent border-b-4 border-purple-600 min-w-64"
       />
-      <button onClick={handleSearch}>SEARCH</button>
-    </>
+      <button
+        onClick={handleSearch}
+        className="mt-8 text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        SEARCH
+      </button>
+    </div>
   );
 }
 
